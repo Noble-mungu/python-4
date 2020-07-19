2d9c4c096b4d6b49d0a919d320f1c0f8f5791601
Noble Mungu <mungunoble@gmail.com>
2020-07-19 15:12:35 +0300

    add pitchform class

--- app/main/form.py
+++ app/main/form.py
@@ -5,3 +5,9 @@ from wtforms.validators import Required
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Write a brief bio about you.',validators = [Required()])
     submit = SubmitField('Save')
+
+class PitchForm(FlaskForm):
+    title = StringField('Title', validators=[Required()])
+    category = SelectField('Category', choices=[('Events','Events'),('Job','Job'),('Advertisement','Advertisement')],validators=[Required()])
+    post = TextAreaField('Your Pitch', validators=[Required()])
+    submit = SubmitField('Pitch')
