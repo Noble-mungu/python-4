2a16c02237d8e373512f630b80771facfc7641ea
Noble Mungu <mungunoble@gmail.com>
2020-07-19 15:23:41 +0300

    add update pic class route

--- app/main/views.py
+++ app/main/views.py
@@ -62,4 +62,14 @@ def updateprofile(name):
         user.save_u()
         return redirect(url_for('.profile',name = name))
     return render_template('profile/update.html',form =form)
+@main.route('/user/<name>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(name):
+    user = User.query.filter_by(username = name).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',name=name))
     
