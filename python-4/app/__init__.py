911ac1cdb71d2a70d59c7c029770d9a3b7f1879d
Noble Mungu <mungunoble@gmail.com>
2020-07-25 22:34:50 +0300

    update class user model

--- python-4/app/__init__.py
+++ python-4/app/__init__.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
 bootstrap = Bootstrap()
-db =SQLAlchemy()
+db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
@@ -13,7 +13,7 @@ login_manager.login_view = 'auth.login'
 def create_app(config_name):
     app = Flask(__name__)
 
-    app.config.from_object(config_name)
+    app.config.from_object(Config)
 
 
     from .main import main as main_blueprint
--- python-4/manage.py
+++ python-4/manage.py
@@ -1,4 +1,4 @@
-from app import create_app
+from app import create_app,db
 from flask_script import Manager,Server
 from app.models import User
 
@@ -6,7 +6,7 @@ from app.models import User
 app = create_app()
 
 manager = Manager(app)
-manager.add_command('server',Server(host='localhost',port=1234))
+manager.add_command('server',Server(use_debugger=True))
 
 @manager.add_command
 def test():
