147826cb0742939df56abdf5d9e951ee92af285d
Noble Mungu <mungunoble@gmail.com>
2020-07-26 12:52:13 +0300

    add profile function

--- python-4/app/main/views.py
+++ python-4/app/main/views.py
@@ -4,5 +4,40 @@ from ..models import User
 
 @main.route('/')
 def index():
-	return render_template('index.html')
+	quotes = get_quotes('page',1,type = int)
+	page = request.args.get('page',1,type = int)
+	blogs = Blog.query.order_by(Blog.posted.desc()).paginate(page = page ,per_page = 3)
+	return render_template('index.html',quote = quote,blogs=blogs)
+def save_picture(form_picture):
+	random_hex = secrets.token_hex(8)
+	_,f_ext = os.path.splitext(form_picture.filename)
+	picture_filename = random_hex + f_ext
+	picture_path = os.path.join('app/static/photos')
 
+	output_size = (180,180)
+	i = Image.open(form_picture)
+	i.thumbnail(output_size)
+	i.save(picture_path)
+	return picture_filename
+
+
+@main.route('/profile',methods = ['POST','GET'])
+@login_required
+def profile():
+    form = UpdateProfile()
+    if form.validate_on_submit():
+        if form.profile_picture.data:
+            picture_file = save_picture(form.profile_picture.data)
+            current_user.profile_pic_path = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        current_user.bio = form.bio.data
+        db.session.commit()
+        flash('Succesfully updated your profile')
+        return redirect(url_for('main.profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+        form.bio.data = current_user.bio
+    profile_pic_path = url_for('static',filename = 'photos/'+ current_user.profile_pic_path) 
+   
