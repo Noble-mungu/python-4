021e35e2ead47667bbac2ac2dfa4eba2c6cc5345
Noble Mungu <mungunoble@gmail.com>
2020-08-24 09:45:42 +0300

    add upvotes and downvotes in quote file

--- life-quotes/life-quotes/src/app/quote.ts
+++ life-quotes/life-quotes/src/app/quote.ts
@@ -1,13 +1,13 @@
 export class Quote {
-
-
-	showDescription:boolean
-	constructor(
-	public quote:string,
-	public author:string,
-	public poster:string,
-
-	){
-		this.showDescription=false;
-	}
-	}
+    showDescription: boolean;
+    constructor(
+        public quote: string,
+        public author: string,
+        public poster: string,
+        public upvotes: number,
+        public downvotes: number,
+        public completeDate: Date,
+    ){ 
+        this.showDescription=false;
+    }    
+}
