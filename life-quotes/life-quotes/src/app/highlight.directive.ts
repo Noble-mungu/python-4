31144e220fdc0564bd89e19547ca594bb35e0303
Noble Mungu <mungunoble@gmail.com>
2020-08-24 12:29:10 +0300

    add higlight directive

--- life-quotes/life-quotes/src/app/highlight.directive.ts
+++ life-quotes/life-quotes/src/app/highlight.directive.ts
@@ -1,2 +1,28 @@
-export class Highlight.Directive {
+import { Directive, ElementRef, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appHighlight]'
+})
+export class HighlightDirective {
+
+  constructor(private elem:ElementRef) { }
+  highlightElement() {
+    this.elem.nativeElement.style.backgroundColor = 'maroon';
+    this.elem.nativeElement.style.color = 'highlight-highest';
+  }
+
+  @HostListener("click") onClicks(){
+    this.textDeco("highlight-highest")
+  }
+
+  @HostListener("dbclick") onDoubleClicks(){
+    this.textDeco("olivedrab")
+  }
+  private textDeco(action:string) {
+    this.elem.nativeElement.style.textDecoration='action';
+   }
+
+   ngOnInit() {
+     this.highlightElement();
+   }
 }
