095668aadc4baa312e6600a8911519bbb7c57509
Noble Mungu <mungunoble@gmail.com>
2020-08-24 12:47:46 +0300

    add form component

--- life-quotes/life-quotes/src/app/app.module.ts
+++ life-quotes/life-quotes/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
-    QuoteDetailComponent
+    QuoteDetailComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule
