376fabe9d481c7abaa7d3d9959e5ec9ea752792c
Noble Mungu <mungunoble@gmail.com>
2020-08-24 09:43:41 +0300

    add highlight directive

--- life-quotes/life-quotes/src/app/quote/quote.component.ts
+++ life-quotes/life-quotes/src/app/quote/quote.component.ts
@@ -33,10 +33,11 @@ export class QuoteComponent implements OnInit {
   submitType = 'save';
 
    quote = [
-    new Quote("Hardwork pays", "English", "Proverb", 0, 0, new Date(2010,6,13)),
-    new Quote("Mtaka cha mvunguni sharti ainame", "Ken", "Walibora", 0, 0, new Date(2019,6,4)),
-    new Quote("Tout est beau tant que tu l'impose", "Willy William", "Mozzy", 0, 0, new Date(2012,7,21)),
-    new Quote("Yaliyopita si ndwele", "KIswahili", "Proverb", 0, 0, new Date(2019,8,10))
+   new Quote("Inauma itabidi uzoee","Simple Boy","SWahili"),
+	new Quote("Ooliskia wapi","Ule Msee","Says"),
+	new Quote("Hardwork pays","English","Proverb"),
+	new Quote("Yaliyopita si ndewle","Swahili","Proverb"),
+
   ];
 
   deleteQuote(isComplete, index){
